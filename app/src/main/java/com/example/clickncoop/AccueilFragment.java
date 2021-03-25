4f8e1e7dc580fd14944a6fa202ae31277c4bc7a3
Gaëlle L <gaelle.lajotsarthou@outlook.fr>
2021-03-25 16:12:25 +0100

    Page d'accueil

--- app/src/main/java/com/example/clickncoop/AccueilFragment.java
+++ app/src/main/java/com/example/clickncoop/AccueilFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class AccueilFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,17 +17,25 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.accueil_fragment, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.mash).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
+                NavHostFragment.findNavController(AccueilFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+
+        view.findViewById(R.id.rythm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(AccueilFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_rythmFragment);
             }
         });
     }
---
+++ app/src/main/java/com/example/clickncoop/RythmFragment.java
@@ -0,0 +1,36 @@
+package com.example.clickncoop;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RythmFragment extends Fragment {
+
+    private RythmViewModel mViewModel;
+
+    public static RythmFragment newInstance() {
+        return new RythmFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.rythm_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RythmViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
