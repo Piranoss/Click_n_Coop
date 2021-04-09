4e567ed4b70fe79b67f6ad8af6c87359e6d27f6e
Gaëlle L <71330270+Shilizen@users.noreply.github.com>
2021-04-09 21:29:34 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/clickncoop/MainActivity.java
+++ app/src/main/java/com/example/clickncoop/MainActivity.java
@@ -7,6 +7,9 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.View;
 
@@ -21,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     @Override
@@ -41,5 +43,42 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void replaceFragment(Fragment fragment, String tag) {
+        //Place le fragment actuel dans le container
+        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.container);
+
+        //Prévient d'ajouter le même fragment à la tête de la pile
+        if (currentFragment.getClass() == fragment.getClass()) {
+            return;
+        }
+
+        //Si le fragment est déjà sur la pile, nous pouvons remettre la pile en place pour éviter une croissance infinie de la pile.
+        if (getSupportFragmentManager().findFragmentByTag(tag) != null) {
+            getSupportFragmentManager().popBackStack(tag, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+
+        //Sinon, juste replacer le fragment.
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack(tag)
+                .replace(R.id.container, fragment, tag)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        CountViewModel viewModel = new ViewModelProvider(this).get(CountViewModel.class);
+        viewModel.setCounterMash(0);
+        viewModel.setCounterRythm(0);
+        int fragmentsInStack = getSupportFragmentManager().getBackStackEntryCount();
+        if (fragmentsInStack > 1) { // Si nous avons plus d'un fragment, remonter la pile.
+            getSupportFragmentManager().popBackStack();
+        } else if (fragmentsInStack == 1) { // Terminé l'activité, s'il ne reste qu'un seul fragment, pour éviter de laisser un écran vide.
+            finish();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
